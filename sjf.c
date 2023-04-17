6f267a965da1e6d07849751cf3751caae78d1cb6
EDWIN ALEX SHAJI <96735074+edwineas@users.noreply.github.com>
2023-04-17 11:48:44 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -4,7 +4,7 @@ int main()
  	int n;
 	printf("Enter the number of process to be inserted : ");
 	scanf("%d",&n);
-	int a[n][5], i, j, temp;
+	int a[n][5], i, j, temp, cmpt, s, k;
 	float  atat, awt, ttat=0, twt=0;
 	for(i=0;i<n;i++) //collecting process details
 	{
@@ -15,45 +15,70 @@ int main()
 	{
 		for(j=0;j<n-1-i;j++)
 		{
-			if(a[j][2]>a[j+1][2])
+			if(a[j][1]>a[j+1][1])
 			{
-				temp=a[j][1];
-				a[j][1]=a[j+1][1];
-				a[j+1][1]=temp;
-				temp=a[j][2];
-				a[j][2]=a[j+1][2];
-				a[j+1][2]=temp;
+				for(k=1;k<=2;k++)
+				{
+					temp=a[j][k];
+					a[j][k]=a[j+1][k];
+					a[j+1][k]=temp;
+				}
 				
 			}		
 		}
 	}
-	if(a[0][1]!=0)
-		a[0][3]=a[0][2]+a[0][1];
-	else
-		a[0][3]=a[0][2]; 
-	for(i=1;i<n;i++) // completion time
+	a[0][3]=a[0][1]+a[0][2];
+	a[0][4]=a[0][3]-a[0][1];
+	a[0][5]=a[0][4]-a[0][2];
+ 	for(i=1;i<n-1;i++) //sorting in burst time process
+	{
+		for(j=1;j<n-1-i;j++)
+		{
+			if(a[j][2]>a[j+1][2])
+			{
+				for(k=1;k<=2;k++)
+				{
+					temp=a[j][k];
+					a[j][k]=a[j+1][k];
+					a[j+1][k]=temp;
+				}
+				
+			}		
+		}
+	}
+	for(i=1;i<n;i++)
 	{
 		a[i][3]=a[i-1][3]+a[i][2];
-	}
-	for(i=0;i<n;i++) // turnaround time
-	{
 		a[i][4]=a[i][3]-a[i][1];
-	}
-	for(i=0;i<n;i++) //waiting time
-	{
 		a[i][5]=a[i][4]-a[i][2];
 	}
-	for(i=0;i<n;i++) //total turnaround time and waiting time
+	for(i=0;i<n;i++)
 	{
 		ttat=ttat+a[i][4];
 		twt=twt+a[i][5];
 	}
-	atat=ttat/n; //average turnaround time and waiting time
+	atat=ttat/n;
 	awt=twt/n;
-	printf("P\tat\tbt\ttat\tt\n");
+	for(i=0;i<n-1;i++) //sorting process
+	{
+		for(j=0;j<n-1-i;j++)
+		{
+			if(a[j][1]>a[j+1][1])
+			{
+				for(k=1;k<=5;k++)
+				{
+					temp=a[j][k];
+					a[j][k]=a[j+1][k];
+					a[j+1][k]=temp;
+				}
+				
+			}		
+		}
+	}
+	printf("P\tat\tbt\tcmpt\ttat\twt\n");
 	for(i=0;i<n;i++) //printing
 	{
-		printf("%d\t%d\t%d\t%d\t%d\n",i,a[i][1],a[i][2],a[i][4],a[i][5]);
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",i,a[i][1],a[i][2],a[i][3],a[i][4],a[i][5]);
 	}
 	printf("awt:%f | atat:%f\n",awt,atat);
 } 
