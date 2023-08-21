c374218ee280cacb9c067c3086632c0bb7927895
edwineas <edwinalexshaji@gmail.com>
2023-08-21 09:01:50 +0530

    exe delete

--- tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
- printf("Page faults: %d\n", pageFaults);
-    printf("Page Hits: %d\n", pageHits);
-    printf("Page Fault Ratio: %.2f%%\n", (float)pageFaults * 100 / numPages);
-    printf("Page Hit Ratio: %.2f%%\n", (float)pageHits * 100 / numPages);
