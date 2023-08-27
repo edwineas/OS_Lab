a8e46fe38cfe9df1ee3d03dc1671350a2e0b61ec
edwineas <edwinalexshaji@gmail.com>
2023-08-27 08:57:14 +0530

    final

--- EXP.c
+++ EXP.c
@@ -1,99 +1,55 @@
+// Title: FIFO Page Replacement Algorithm
 #include <stdio.h>
-int main()
-{
- 	int n;
-	printf("Enter the number of process to be inserted : ");
-	scanf("%d",&n);
-	int a[n][5], i, j, temp, cmpt, s, k;
-	float  atat, awt, ttat=0, twt=0;
-	for(i=0;i<n;i++) //collecting process details
-	{
-		printf("Enter the arrival time and burst time of P%d : ",i);
-		scanf("%d%d",&a[i][1],&a[i][2]);
-	}
-	for(i=0;i<n-1;i++) //sorting process
-	{
-		for(j=0;j<n-1-i;j++)
-		{
-			if(a[j][1]>a[j+1][1])
-			{
-				for(k=1;k<=2;k++)
-				{
-					temp=a[j][k];
-					a[j][k]=a[j+1][k];
-					a[j+1][k]=temp;
-				}
-				
-			}		
-		}
-	}
-	a[0][3]=a[0][1]+a[0][2];
-	a[0][4]=a[0][3]-a[0][1];
-	a[0][5]=a[0][4]-a[0][2];
 
- 	for(i=1;i<n-1;i++) //sorting in burst time process
-	{
-		for(j=1;j<=n-1-i;j++)
-		{
-			if(a[j][2]>a[j+1][2])
-			{
-				for(k=1;k<=2;k++)
-				{
-					temp=a[j][k];
-					a[j][k]=a[j+1][k];
-					a[j+1][k]=temp;
-				}
-				
-			}		
-		}
-	}
-	for(i=1;i<n;i++)
-	{
-		a[i][3]=a[i-1][3]+a[i][2];
-		a[i][4]=a[i][3]-a[i][1];
-		a[i][5]=a[i][4]-a[i][2];
-	}
-	for(i=0;i<n;i++)
-	{
-		ttat=ttat+a[i][4];
-		twt=twt+a[i][5];
-	}
-	atat=ttat/n;
-	awt=twt/n;
-	for(i=0;i<n-1;i++) //sorting process
-	{
-		for(j=0;j<n-1-i;j++)
-		{
-			if(a[j][1]>a[j+1][1])
-			{
-				for(k=1;k<=5;k++)
-				{
-					temp=a[j][k];
-					a[j][k]=a[j+1][k];
-					a[j+1][k]=temp;
-				}
-			}		
-		}
-	}
-	printf("P\tat\tbt\tcmpt\ttat\twt\n");
-	for(i=0;i<n;i++) //printing
-	{
-		printf("%d\t%d\t%d\t%d\t%d\t%d\n",i,a[i][1],a[i][2],a[i][3],a[i][4],a[i][5]);
-	}
-	printf("awt:%f | atat:%f\n",awt,atat);
-} 
+int main() {
+    int referenceString[100], numPages, numFrames, frames[10], pageFaults = 0, frameIndex = 0;
 
-/*
-Enter the number of processes to be inserted: 4
-Enter the arrival time and burst time of P0: 0 5
-Enter the arrival time and burst time of P1: 1 3
-Enter the arrival time and burst time of P2: 2 8
-Enter the arrival time and burst time of P3: 3 6
-P	at	bt	cmpt	tat	wt
-0	0	5	5	5	0
-1	1	3	8	7	4
-2	2	6	14	12	6
-3	3	8	22	19	11
-awt:6.750000 | atat:10.750000
+    printf("Enter the number of pages: ");
+    scanf("%d", &numPages);
 
-*/
+    printf("Enter the reference string: ");
+    for (int i = 0; i < numPages; i++)
+        scanf("%d", &referenceString[i]);
+
+    printf("Enter the number of frames: ");
+    scanf("%d", &numFrames);
+
+    for (int i = 0; i < numFrames; i++) { 
+        frames[i] = -1; // -1 indicates empty frame
+    }
+
+    for (int i = 0; i < numPages; i++) { // for each page in reference string
+        int pageFound = 0; // 0 indicates page not found
+
+        for (int j = 0; j < numFrames; j++) {
+            if (frames[j] == referenceString[i]) { // if page found in frame
+                pageFound = 1; // 1 indicates page found
+                break;
+            }
+        }
+
+        if (!pageFound) { // if page not found in frame
+            frames[frameIndex] = referenceString[i];
+            frameIndex = (frameIndex + 1) % numFrames;
+            pageFaults++;
+        }
+
+        printf("RS: %d | ", referenceString[i]);
+        for (int j = 0; j < numFrames; j++) {
+            if (frames[j] == -1)
+                printf("_ ");
+            else
+                printf("%d ", frames[j]);
+        }
+        printf("\n");
+    }
+
+    int pageHits = numPages - pageFaults;
+
+    printf("Page faults: %d\n", pageFaults);
+    printf("Page Hits: %d\n", pageHits);
+    printf("Page Fault Ratio: %.2f%%\n", (float)pageFaults * 100 / numPages);
+    printf("Page Hit Ratio: %.2f%%\n", (float)pageHits * 100 / numPages);
+
+    return 0;
+}
